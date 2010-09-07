c8e1d0f90171ea3d30fe0c84c14cef8ed71f9802
Brad Robinson <brobinson@toptensoftware.com>
2010-09-07 17:29:01 +1000

    Added disclaimer comments

--- MarkdownDeep/HtmlTag.cs
+++ MarkdownDeep/HtmlTag.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// 
+//   MarkdownDeep - http://www.toptensoftware.com/markdowndeep
+// 
+//   The contents of this file are subject to the license terms as 
+//	 specified at the web address above.
+//  
+//   Software distributed under the License is distributed on an 
+//   "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+//   implied. See the License for the specific language governing
+//   rights and limitations under the License.
+// 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- MarkdownDeep/Properties/AssemblyInfo.cs
+++ MarkdownDeep/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DarkMarkdown")]
+[assembly: AssemblyTitle("MarkdownDeep")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DarkMarkdown")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Topten Software")]
+[assembly: AssemblyProduct("MarkdownDeep")]
+[assembly: AssemblyCopyright("Copyright © Topten Software.  Some Rights Reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.*")]
 
 [assembly: InternalsVisibleTo("MarkdownDeepTests")]
